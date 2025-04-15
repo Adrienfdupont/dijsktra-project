66ac624c172236b65f8f141edf902703898c56f3
Adrienfdupont <adrienf.dupont@gmail.com>
2025-04-15 23:49:58 +0200

    find shortest path

--- src/graph_manager.py
+++ src/graph_manager.py
@@ -3,6 +3,7 @@ import os
 from src.node import Node
 from src.edge import Edge
 from src.settings import *
+from src.priority_queue import PriorityQueue
 
 class GraphManager:
     def __init__(self):
@@ -24,6 +25,11 @@ class GraphManager:
             codes = edge['properties']['codes']
             weight = edge['properties']['weight']
             self.add_edge(codes, weight, pos1, pos2)
+
+            self.graph[codes[0]][codes[1]] = weight
+            self.graph[codes[1]][codes[0]] = weight
+            
+        print(self.find_shortest_path('LFYG', 'LFQJ'))
 
     def add_node(self, code, pos):
         node = Node(code, pos)
@@ -47,3 +53,31 @@ class GraphManager:
         x_screen = int(x_ratio * WIDTH)
         y_screen = int(y_ratio * HEIGHT)
         return x_screen, y_screen
+    
+    def find_shortest_path(self, start, end):
+        previous = {v: None for v in self.graph.keys()}
+        visited = {v: False for v in self.graph.keys()}
+        distances = {v: float("inf") for v in self.graph.keys()}
+        distances[start] = 0
+        queue = PriorityQueue()
+        queue.add_task(0, start)
+
+        while queue:
+            removed_distance, removed = queue.pop_task()
+            visited[removed] = True
+
+            for node, distance in self.graph[removed].items():
+                if visited[node]:
+                    continue
+                new_distance = removed_distance + distance
+                if new_distance < distances[node]:
+                    distances[node] = new_distance
+                    previous[node] = removed
+                    queue.add_task(new_distance, node)
+        path = []
+        current = end
+        while current is not None:
+            path.insert(0, current)
+            current = previous[current]
+
+        return path
---
+++ src/priority_queue.py
@@ -0,0 +1,35 @@
+import itertools
+from heapq import heappush, heappop
+
+class PriorityQueue:
+    def __init__(self):
+        self.pq = []
+        self.entry_finder = {}
+        self.counter = itertools.count()
+
+    def __len__(self):
+        return len(self.pq)
+
+    def add_task(self, priority, task):
+        'Add a new task or update the priority of an existing task'
+        if task in self.entry_finder:
+            self.update_priority(priority, task)
+            return self
+        count = next(self.counter)
+        entry = [priority, count, task]
+        self.entry_finder[task] = entry
+        heappush(self.pq, entry)
+
+    def update_priority(self, priority, task):
+        'Update the priority of a task in place'
+        entry = self.entry_finder[task]
+        count = next(self.counter)
+        entry[0], entry[1] = priority, count
+
+    def pop_task(self):
+        'Remove and return the lowest priority task. Raise KeyError if empty.'
+        while self.pq:
+            priority, count, task = heappop(self.pq)
+            del self.entry_finder[task]
+            return priority, task
+        raise KeyError('pop from an empty priority queue')
